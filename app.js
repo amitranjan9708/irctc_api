cd4ede87e4437e4168201b05bcbb5e7e9ae0fff9
amitranjan9708 <106347427+amitranjan9708@users.noreply.github.com>
2024-12-31 02:46:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { authenticateToken, checkAdminApiKey } = require('./middleware/auth');
 const userRoutes = require('./routes/userRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
-const bookingdetailsRoutes = require('./routes/bookingDetailsRoutes');
+// const bookingdetailsRoutes = require('./routes/bookingDetailsRoutes');
 const trainRoutes = require('./routes/trainRoutes');
 const dotenv = require('dotenv');
 
@@ -21,7 +21,7 @@ app.use(cors());
 app.use('/api', userRoutes);
 app.use('/api/admin', checkAdminApiKey, adminRoutes);
 app.use('/api', authenticateToken, bookingRoutes);
-app.use('/api', authenticateToken, bookingdetailsRoutes);
+// app.use('/api', authenticateToken, bookingdetailsRoutes);
 app.use('/api', trainRoutes);
 
 // Start server
