eb8d470c76aea1e3186f80d73997108154625c00
amitranjan9708 <106347427+amitranjan9708@users.noreply.github.com>
2024-12-31 02:26:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port = 3306  // Avoid port 3306
+const port = 3000  // Avoid port 3306
 
 app.use(bodyParser.json());
 app.use(cors());
