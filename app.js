4d9361ac6f4b6145ae27a3584b79b72dab5d478a
amitranjan9708 <106347427+amitranjan9708@users.noreply.github.com>
2024-12-31 00:53:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const trainRoutes = require('./routes/trainRoutes');
 const app = express();
 
 dotenv.config();
-const PORT = process.env.port||3000;
+const port = process.env.PORT||3000;
 
 
 
@@ -27,5 +27,5 @@ app.use('/api', authenticateToken, bookingdetailsRoutes);
 app.use('/api', trainRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
