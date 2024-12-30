3dc1e49128cf216b03fb2d1d6d87c5f238ebd3a5
amitranjan9708 <106347427+amitranjan9708@users.noreply.github.com>
2024-12-31 01:44:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,27 +5,26 @@ const { authenticateToken, checkAdminApiKey } = require('./middleware/auth');
 const userRoutes = require('./routes/userRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
-const dotenv = require('dotenv')
 const bookingdetailsRoutes = require('./routes/bookingDetailsRoutes');
 const trainRoutes = require('./routes/trainRoutes');
-// const mysql = require('mysql2')
-const app = express();
+const dotenv = require('dotenv');
 
 dotenv.config();
-const port = 3306;
 
-
-
+const app = express();
+const port = process.env.APP_PORT || 3000; // Avoid port 3306
 
 app.use(bodyParser.json());
 app.use(cors());
 
+// Define routes
 app.use('/api', userRoutes);
 app.use('/api/admin', checkAdminApiKey, adminRoutes);
 app.use('/api', authenticateToken, bookingRoutes);
 app.use('/api', authenticateToken, bookingdetailsRoutes);
 app.use('/api', trainRoutes);
 
+// Start server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
